d6c20166b40e3080de6faf8024bcc8de502f4c1f
MrKickkiller <mathieu_coussens@hotmail.com>
2015-10-04 00:34:05 +0200

    Push referance back to Dev

---
+++ src/main/java/com/blockingHD/Referance.java
@@ -0,0 +1,16 @@
+package com.blockingHD;
+
+import java.lang.String;
+
+public class Referance {
+    public static final String NAME = "BlockingBot";
+    public static final String LONGIN = "BlockingBot";
+    public static final String HOST = "irc.twitch.tv";
+    public static final String CHAN = "#loneztar";
+    public static final String PASS = "";
+
+    public static final int PORT = 6667;
+
+
+
+}
