0e79599a028ca38f3c72b18dca7ee9e133a2453f
blockingHD <blockingHD@gmail.com>
2015-10-03 20:09:34 +0100

    Mainclass and ref

--- src/main/java/com/blockingHD/Referance.java
+++ src/main/java/com/blockingHD/Referance.java
@@ -7,8 +7,7 @@ public class Referance {
     public static final String LONGIN = "BlockingBot";
     public static final String HOST = "irc.twitch.tv";
     public static final String CHAN = "#blockinghd";
-    public static final String PASS = "oauth:xfheqwn84xaigbfav24nlzae9664x9";
-    public static final String clientid = "7sueekkoq13sakeee34vc7wqn4630kw";
+    public static final String PASS = "";
 
     public static final int PORT = 6667;
 
