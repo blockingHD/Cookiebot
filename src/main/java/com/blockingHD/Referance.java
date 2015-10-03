b6f1ba65b3efb4b8d7d1ba6d30d3e3b619999b56
blockingHD <blockingHD@gmail.com>
2015-10-03 20:07:10 +0100

    Added main class and referance.

---
+++ src/main/java/com/blockingHD/Referance.java
@@ -0,0 +1,17 @@
+package com.blockingHD;
+
+import java.lang.String;
+
+public class Referance {
+    public static final String NAME = "BlockingBot";
+    public static final String LONGIN = "BlockingBot";
+    public static final String HOST = "irc.twitch.tv";
+    public static final String CHAN = "#blockinghd";
+    public static final String PASS = "oauth:xfheqwn84xaigbfav24nlzae9664x9";
+    public static final String clientid = "7sueekkoq13sakeee34vc7wqn4630kw";
+
+    public static final int PORT = 6667;
+
+
+
+}
