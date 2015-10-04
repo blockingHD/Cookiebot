01424219fd3f1938a9512a74a1ef62d4758b250e
MrKickkiller <mathieu_coussens@hotmail.com>
2015-10-05 00:24:24 +0200

    More database manipulations.

--- src/main/java/com/blockingHD/games/guess.java
+++ src/main/java/com/blockingHD/games/guess.java
@@ -1,5 +1,6 @@
 package com.blockingHD.games;
 
+import com.blockingHD.CookieBotMain;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -21,25 +22,28 @@ public class guess extends ListenerAdapter<PircBotX> {
     public void onMessage(MessageEvent<PircBotX> event) throws Exception {
         String username = event.getUser().getNick();
 
-        if (event.getMessage().toLowerCase().contains("!resetguess")){
+        if (event.getMessage().toLowerCase().contains("!resetguess") && CookieBotMain.CDBM.getModStatusForPerson(username)){
             rand = new Random().nextInt(30);
             System.out.print(rand);
             users.clear();
             isDone = false;
+            event.getChannel().send().message("A new round of cookie-guessing has begon. Get to them before Loneztar does!");
 
-        }else if (event.getMessage().toLowerCase().contains("!guess")){
+        }else if (!CookieBotMain.CDBM.getModStatusForPerson(username)&& event.getMessage().toLowerCase().contains("!resetguess")){
+            event.getChannel().send().message("Nice try " + username + ", but better luck next time!");
+        } else if (event.getMessage().toLowerCase().contains("!guess")){
             String guess = event.getMessage().replace("!guess ", "");
             if (isInt(guess)) {
                 if (rand != Integer.parseInt(guess) && !isDone && !users.contains(username)) {
-                    event.getChannel().send().message("Sorry that is incorrect " + username + " better look next time.");
+                    event.getChannel().send().message("Sorry that is incorrect " + username + " better luck next time.");
                     users.add(username);
                 } else if (rand == Integer.parseInt(guess) && !isDone && !users.contains(username)) {
-                    event.getChannel().send().message("That is correct " + username + " here have " + rand + " cookies.");
+                    event.getChannel().send().message("That is correct " + username + "! Here have " + rand + " cookies.");
                     isDone = true;
                 } else if (isDone) {
-                    event.getChannel().send().message("Sorry the cookies have been taken better look next time " + username + ".");
+                    event.getChannel().send().message("Sorry the cookies have been taken ,better luck next time " + username + ".");
                 } else if (users.contains(username)) {
-                    event.getChannel().send().message("You have already guess " + username + ".");
+                    event.getChannel().send().message("You have already guessed " + username + ".");
                 } else {
                     System.out.println("ERROR!");
                 }
