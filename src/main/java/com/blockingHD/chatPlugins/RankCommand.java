b81b3dbfbe3263e1318e71d4c4901163b78698b1
MrKickkiller <mathieu_coussens@hotmail.com>
2015-10-13 00:24:28 +0200

    1 Bugfix + some comments

--- src/main/java/com/blockingHD/chatPlugins/RankCommand.java
+++ src/main/java/com/blockingHD/chatPlugins/RankCommand.java
@@ -18,14 +18,16 @@ import java.util.TreeMap;
  * Created by MrKickkiller on 7/10/2015.
  */
 public class RankCommand extends ListenerAdapter<PircBotX> {
-    static TreeMap<Integer,String> map = new TreeMap<>();
 
+    static TreeMap<Integer,String> map = new TreeMap<>();
+    boolean enabled = Boolean.parseBoolean(CookieBotMain.prop.getProperty("ranksEnabled"));
     public RankCommand() {
         String url = "ranks";
         if (CookieBotMain.devModeOn){
             url = "src/main/resources/" + url;
         }
         Ranks ranks = null;
+        // Load in the Java-Objects from XML
         try {
             JAXBContext jc = JAXBContext.newInstance(Ranks.class);
             Unmarshaller unmarshaller = jc.createUnmarshaller();
@@ -38,13 +40,14 @@ public class RankCommand extends ListenerAdapter<PircBotX> {
             return;
         }
 
+        // Transform the Java-Objects into the map used
         for (Rank rank : ranks.getRanks()){
             map.put(rank.getMinimum(),rank.getName());
         }
 
     }
 
-    boolean enabled = Boolean.parseBoolean(CookieBotMain.prop.getProperty("ranksEnabled"));
+
 
     @Override
     public void onMessage(MessageEvent<PircBotX> event) throws Exception {
@@ -52,7 +55,7 @@ public class RankCommand extends ListenerAdapter<PircBotX> {
             return;
         }
 
-        //Shows someones rank based for there cookie count.
+        //Shows someone's rank based on their cookie count
         if (event.getMessage().startsWith("!rank")){
             String username = event.getUser().getNick();
             username = username.replace("!rank","").trim().split(" ")[0].toLowerCase();
@@ -62,9 +65,11 @@ public class RankCommand extends ListenerAdapter<PircBotX> {
                 while ( (Integer) map.descendingKeySet().toArray()[tracker] > amountOfCookies){
                     tracker ++;
                 }
+                // Credit :P
                 if (username.equals("blockinghd") || username.equals("mrkickkiller")) {
                     event.getChannel().send().message(username + " is my creator!");
-                }else if (username.equals("loneztar")) {
+                // Channel name now automatically finds broadcaster.
+                }else if (username.equals(event.getChannel().getName().toLowerCase().trim().replace("#",""))) {
                     event.getChannel().send().message(username + " is GOD!");
                 }else {
                     event.getChannel().send().message(username + " is a " + map.get(map.descendingKeySet().toArray()[tracker]));
