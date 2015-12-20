a41d3ffc8034189bb30c3a8dd1360835ee2090b1
MrKickkiller <mathieu_coussens@hotmail.com>
2015-12-20 13:37:52 +0100

    Added a better timer system Credits: @Jezza

--- src/main/java/com/blockingHD/utils/JSONManipulator.java
+++ src/main/java/com/blockingHD/utils/JSONManipulator.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,36 +16,69 @@ import java.util.ArrayList;
 public class JSONManipulator {
 
     public static ArrayList<String> getChatters(String url){
+        InputStream is = null;
         try {
             Gson gson = new Gson();
-            TwitchApiReturn returnVal = gson.fromJson(new BufferedReader(new InputStreamReader(new URL(url).openStream())), TwitchApiReturn.class);
+            URL u = new URL(url);
+            is = u.openStream();
+            TwitchApiReturn returnVal = gson.fromJson(new BufferedReader(new InputStreamReader(is)), TwitchApiReturn.class);
             return returnVal.getChatters().getAllViewersInChat();
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            if (is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return null;
     }
 
     public static ArrayList<String> getModerators(String url){
+        InputStream is = null;
         try {
             Gson gson = new Gson();
-            TwitchApiReturn returnVal = gson.fromJson(new BufferedReader(new InputStreamReader(new URL(url).openStream())), TwitchApiReturn.class);
+            URL u = new URL(url);
+            is = u.openStream();
+            TwitchApiReturn returnVal = gson.fromJson(new BufferedReader(new InputStreamReader(is)), TwitchApiReturn.class);
             return returnVal.getChatters().getModerators();
         }catch (Exception e){
             e.printStackTrace();
+        }finally {
+            if (is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return null;
     }
 
     public static boolean isStreamLive (String url){
+        InputStream inputStream = null;
         try {
             Gson gson = new Gson();
-            KrakenStreamReturn streamReturn = gson.fromJson(new BufferedReader(new InputStreamReader(new URL(url).openStream())), KrakenStreamReturn.class);
-            return streamReturn.stream != null ||streamReturn.getStream() != null;
+            URL urlU = new URL(url);
+            inputStream = urlU.openStream();
+            KrakenStreamReturn streamReturn = gson.fromJson(new BufferedReader(new InputStreamReader(inputStream)), KrakenStreamReturn.class);
+            return streamReturn.stream != null || streamReturn.getStream() != null;
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (inputStream != null){
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return false;
     }
